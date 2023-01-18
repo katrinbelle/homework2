d25abdbfb176d8d9c1b9316cbd7f399fc53d85c8
Ekaterina <kat.bommon@gmail.com>
2023-01-18 16:13:43 +0300

    completing tasks

--- homework/Program.cs
+++ homework/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 ->-> 1*/
+
+int GetValue ()
+{
+    Console.Write("Введите трехзначное число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+int SecontNumber(int number)
+{
+number = (number/10)%10;
+return number;
+}
+int value= SecontNumber(GetValue());
+ Console.WriteLine("Вторая цифра от введенного числа-> "+" "+value);
