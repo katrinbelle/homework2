bb46c0a3fe33e1963cc7baf4f6fb8d4011212450
Ekaterina <kat.bommon@gmail.com>
2023-01-18 16:58:03 +0300

    completing a tesk2

---
+++ homework2/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6 */
+
+Console.Write("Введите любое число: ");
+ int number = Convert.ToInt32(Console.ReadLine());
+ int TrirdNumber (int number)
+ {
+     while ( number> 999)
+     { number= number/10;}
+    number =number%10;
+    return number;
+ }
+if (number> 99) 
+{
+   number= TrirdNumber(number); 
+   Console.WriteLine("Третья цифра от введенного числа  -> " +number);
+}
+else Console.WriteLine("Третьей цифры нет");
