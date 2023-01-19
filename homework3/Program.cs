d1a91f82a578035df634b3661e6dd2ff0a0d0c94
Ekaterina <kat.bommon@gmail.com>
2023-01-19 13:02:18 +0300

    change file

--- homework3/Program.cs
+++ homework3/Program.cs
@@ -4,6 +4,15 @@
 7 -> да
 1 -> нет */
 Console.Write("Введите цифру недели -> ");
-int number = Convert.ToInt32(Console.ReadLine());
+int GetValue()
+{
+    int  value= Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+int number = GetValue ();
+while (number >7) {
+    Console.Write(number+" не является днем недели. Пожалуйста, введите цифру недели -> ");
+number=GetValue ();
+}
 if (number==6 || number==7) Console.WriteLine("Цифра "+ number+ " является выходным днем!");
 else Console.WriteLine("Цифра "+ number+ " не является выходным днем");
